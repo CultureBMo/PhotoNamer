26e8373d4847ccdf0070130a0c31fdcce2afb5be
CultureBMo <benjaminjmorris@hotmail.com>
2014-08-23 08:43:50 +0100

    Initial commit

---
+++ Photo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ExifLib;
+
+namespace PictureUtil
+{
+    public class Photo
+    {
+        public Photo(string path)
+        {
+            this.Path = path;
+
+            using (ExifReader reader = new ExifReader(path))
+            {
+                // Extract the tag data using the ExifTags enumeration
+                DateTime datePictureTaken;
+                if (reader.GetTagValue<DateTime>(ExifTags.DateTimeDigitized, out datePictureTaken))
+                {
+                    this.DateTaken = datePictureTaken;
+                }
+            }
+        }
+
+        public DateTime DateTaken
+        {
+            get;
+            private set;
+        }
+
+        public string Path
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace PictureUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+
+            // get the path from config
+            var rootPath = ConfigurationManager.AppSettings["Path"];
+
+            // sort the photos by DateTaken
+            var photos = Directory.GetFiles(rootPath, "*.jpg")
+                            .Select(p => new Photo(p))
+                            .OrderBy(x => x.DateTaken).ToList();
+
+            // Rename them
+            RenamePhotos(rootPath, photos);
+
+            stopwatch.Stop();
+            System.Console.WriteLine();
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static void RenamePhotos(string rootPath, List<Photo> photos)
+        {
+            for (int i = 0; i < photos.Count; i++)
+            {
+                var oldFilename = photos[i].Path;
+
+                Console.WriteLine(oldFilename);
+                Console.WriteLine("Taken: {0}", photos[i].DateTaken);
+
+                var newName = GetNewName(i + 1);
+
+                Console.WriteLine("Now called: {0}", newName);
+
+                var newFilename = Path.Combine(rootPath, newName);
+
+                // rename if necessary
+                if (string.Compare(oldFilename, newFilename, StringComparison.InvariantCultureIgnoreCase) != 0)
+                {
+                    File.Copy(oldFilename, newFilename, true);
+                }
+
+                var deleteOriginals = bool.Parse(ConfigurationManager.AppSettings["DeleteOriginals"]);
+
+                if (deleteOriginals)
+                {
+                    Console.WriteLine("Deleting original...");
+                    File.Delete(oldFilename);
+                }
+
+                Console.WriteLine("----------------------------");
+            }
+        }
+
+        private static string GetNewName(int photoIndex)
+        {
+            var formattedNumber = photoIndex.ToString("000");
+
+            return string.Format("100 {0}.jpg", formattedNumber);
+        }
+    }
+}
