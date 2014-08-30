2bae21464c5a83efc9bc1ede892d818e6508508a
CultureBMo <benjaminjmorris@hotmail.com>
2014-08-30 09:26:00 +0100

    Added some error handling around Date extraction

--- Photo.cs
+++ Photo.cs
@@ -1,6 +1,7 @@
 ﻿namespace PhotoNamer
 {
     using System;
+    using System.IO;
     using ExifLib;
 
     public class Photo
@@ -9,14 +10,25 @@
         {
             this.Path = path;
 
-            using (ExifReader reader = new ExifReader(path))
+            try
             {
-                // Extract the tag data using the ExifTags enumeration
-                DateTime datePictureTaken;
-                if (reader.GetTagValue<DateTime>(ExifTags.DateTimeDigitized, out datePictureTaken))
+                using (ExifReader reader = new ExifReader(path))
                 {
-                    this.DateTaken = datePictureTaken;
+                    // Extract the tag data using the ExifTags enumeration
+                    DateTime datePictureTaken;
+                    if (reader.GetTagValue<DateTime>(ExifTags.DateTimeDigitized, out datePictureTaken))
+                    {
+                        this.DateTaken = datePictureTaken;
+                    }
                 }
+            }
+            catch
+            {
+                // couldn't get the Exif data - fall back to Date Modified or Date Created whichever is earliest
+                var fileCreatedDate = File.GetCreationTime(path);
+                var fileModifiedDate = File.GetLastWriteTime(path);
+
+                this.DateTaken = fileCreatedDate < fileModifiedDate ? fileCreatedDate : fileModifiedDate;
             }
         }
 
